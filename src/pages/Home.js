e1cd4b938fecdc2a3d74d13d2e08d363450f4117
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-27 05:36:11 +0600

    integrated by composeWithDevTools()

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Home = () => {
-  const state = useSelector((state) => state);
-  console.log(state);
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       <h1>This is home page</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { createStore } from "redux";
 import productReducer from "./productReducers/productReducer";
 
-const store = createStore(productReducer);
+const store = createStore(productReducer, composeWithDevTools());
 
 export default store;
