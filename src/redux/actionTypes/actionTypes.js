804412fb475940469d81aadc854f43a4719c219a
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-27 05:52:00 +0600

    redux> folders(actions,actionTypes,reducers), store.js

---
+++ src/redux/actionTypes/actionTypes.js
@@ -0,0 +1,1 @@
+export const LOAD_PRODUCT = "LOAD_PRODUCT";
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./productReducers/rootReducer";
+import rootReducer from "./reducers/rootReducer";
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
