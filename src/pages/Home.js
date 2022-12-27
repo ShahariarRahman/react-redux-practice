c480d0f0fabc106e7c5e2efb0dc5ae81c7b976ef
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-27 06:29:49 +0600

    load product by redux thunk && show the product at home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
 import loadProductData from "../redux/thunk/products/fetchProducts";
 
 const Home = () => {
+  const products = useSelector(state => state.product.products);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadProductData())
   }, [dispatch]);
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+      {products.map(product => <ProductCard key={product._id} product={product} />)}
     </div>
   );
 };
