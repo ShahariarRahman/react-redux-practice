44d7fd7fbce3b0c23a120e20c2048cb0b6887b6c
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-28 03:05:18 +0600

    if (action.type === ADD_TO_CART)

--- src/redux/middleware/cartCounter.js
+++ src/redux/middleware/cartCounter.js
@@ -1,5 +1,8 @@
+import { ADD_TO_CART } from '../actionTypes/actionTypes'
 const cartCounter = (store) => (next) => (action) => {
-
+    if (action.type === ADD_TO_CART) {
+        console.log(action);
+    }
     return next(action);
 };
 
